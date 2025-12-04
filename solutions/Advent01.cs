1d4ca45be9d4099a2c63e4a0cb25e80b2ee82c44
dannygallows <43937866+dannygallows@users.noreply.github.com>
2025-12-04 19:26:36 +0100

    @

---
+++ solutions/Advent01.cs
@@ -0,0 +1,38 @@
+﻿int result = 50;
+int count = 0;
+
+using (var reader = new StreamReader("input.txt"))
+{
+    string? line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        int number = int.Parse(line[1..]);
+
+        while (number > 99)
+        { 
+            number -= 100;
+            count++;
+        }
+
+        if (line[0] == 'L')
+        {
+            result -= number;
+            if (result < 0)
+            {
+                result += 100;
+                count++;
+            }
+        }
+        else
+        {
+            result += number;
+            if (result > 99)
+            {
+                result -= 100;
+                count++;
+            }
+        }
+    }
+}
+Console.WriteLine(result);
+Console.WriteLine(count);
